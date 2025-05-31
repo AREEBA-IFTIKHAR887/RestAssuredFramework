2c41c448e6b35184ffc28d72d1e8ed1b07b9450a
AREEBA-IFTIKHAR887 <areebaiftikhar887@gmail.com>
2025-06-01 00:07:44 +0500

    login credentials vis json

--- src/test/java/Testcases/LoginTests.java
+++ src/test/java/Testcases/LoginTests.java
@@ -1,17 +1,21 @@
 package Testcases;
 
 import POJO.Login;
-import POJO.Product;
 import Payloads.Payload;
 import Routes.Routes;
+import Utils.JsonUtils;
 import Utils.RestUtils;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Map;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 
 public class LoginTests extends BaseClass{
+    public static Map<String, Object> dataFromJsonFile;
 
     @Test
     public void testInvalidLogin() {
@@ -23,13 +27,18 @@ public class LoginTests extends BaseClass{
     }
     @Test
     public void testSuccessFullLogin() {
-        String username=configReader.getProperty("username");
-        String password=configReader.getProperty("password");
-        Login newlogin = new Login(username,password) ;
+            try{
+                dataFromJsonFile = JsonUtils.getJsonDataAsMap("logindata.json");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        String username = (String) dataFromJsonFile.get("username");
+        String password = (String) dataFromJsonFile.get("password");
+
+        Login newlogin=new Login(username,password);
         Response response = RestUtils.performPost(Routes.Auth_Login, newlogin);
         response.then()
                 .statusCode(200)
                 .body("token",notNullValue());
     }
-
 }
